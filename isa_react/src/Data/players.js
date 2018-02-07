72ddb927b68a71d0997cc5401fd8a0392ce5edf4
auraya33 <kasia.nagel@gmail.com>
2018-02-07 18:43:57 +0100

    add reactJS

---
+++ isa_react/src/Data/players.js
@@ -0,0 +1,13 @@
+const players = [
+    {id: 1, nickname: 'Jankos', level: 45, about: 'Polish jungle player'},
+    {id: 2, nickname: 'Vander', level: 55, about: 'Polish support player'},
+    {id: 3, nickname: 'Yellowstar', level: 57, about: 'One of best european support players'},
+    {id: 4, nickname: 'Faker', level: 60, about: 'Best midlaner in world'},
+    {id: 5, nickname: 'kaSing', level: 57, about: 'UK support player'},
+    {id: 6, nickname: 'Selfie', level: 20, about: 'Polish midlaner'},
+    {id: 7, nickname: 'Hans Sama', level: 49, about: 'Best european ADC player'},
+    {id: 8, nickname: 'Steelback', level: 33, about: 'French ADC player'},
+    {id: 9, nickname: 'Jactroll', level: 27, about: 'Debutant on LCS scene, polish support player'},
+    {id: 10, nickname: 'Pride', level: 41, about: 'Young jungle player from Netherland'}
+];
+
