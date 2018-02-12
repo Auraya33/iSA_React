5d7398b70c6bd98a3060e1a03367d84db20f500d
auraya33 <kasia.nagel@gmail.com>
2018-02-12 17:11:06 +0100

    add task array for app

---
+++ isa_react_task-12/src/Components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProfider';
+import PropTypes from 'prop-types';
+
+export default class extends React.Component {
+
+    state = {
+        1: {name: 'Do laundry', isFinished: false},
+        2: {name: 'Cook meal', isFinished: true},
+        4: {name: 'Clean dishes', isFinished: true},
+        3: {name: 'Set bed', isFinished: false},
+        5: {name: 'Vaccum floors', isFinished: false},
+        6: {name: 'Take dog for walk ', isFinished: true},
+        7: {name: 'Clean mirror', isFinished: false},
+        8: {name: 'Empty trash', isFinished: true}
+    };
+
+
+    
+}
