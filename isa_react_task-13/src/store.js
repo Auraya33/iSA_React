42625fc81c186946c17c2c61b30b6c4c308d2406
auraya33 <kasia.nagel@gmail.com>
2018-03-12 15:46:00 +0100

    add react-redux

--- isa_react_task-13/src/store.js
+++ isa_react_task-13/src/store.js
@@ -2,25 +2,26 @@ import { createStore } from 'redux';
 
 const initialState = {
     value: 0
-};
-
-const reducer = (state =  initialState, action = {}) => {
+}
+const reducer = (state = initialState, action = {}) => {
     switch (action.type) {
         case '':
             return {
 
-    };
-
-    default:
-        return  state
+            }
+        default:
+            return state
+    }
 }
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__
-);
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-    store.subscribe(() => {
-        console.log('zmiana', store.getState())
-    })
-};
+store.subscribe(() => {
+    console.log('zmiana', store.getState())
+})
+
+
+export default store
